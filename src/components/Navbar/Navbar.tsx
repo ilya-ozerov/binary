98763de326bb1dbc05a3a374a32269fe0b924526
Ilya Ozerov <efwe456lpzx>
2021-10-12 18:14:52 +0300

    added mobile menu in navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './navbar.scss';
 
 type NavbarProps = {
@@ -15,6 +15,19 @@ export const Navbar: React.FC<NavbarProps> = ({
 
     const scrollTo = (el: React.RefObject<HTMLDivElement>) => {
         el.current?.scrollIntoView({behavior: "smooth"});
+
+        setIsBurger(false)
+    }
+
+    const [isBurger, setIsBurger] = useState(false);
+
+    const toggleBurger = (toggleValue: boolean) => {
+        setIsBurger(toggleValue);
+        if (document.body.style.overflow !== "hidden") {
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = "scroll";
+        }
     }
 
     return (
@@ -25,6 +38,30 @@ export const Navbar: React.FC<NavbarProps> = ({
                 <li onClick={() => scrollTo(portfolioRef)}>portfolio</li>
                 <li onClick={() => scrollTo(contactRef)}>contact</li>
             </ul>
+
+            <div onClick={() => setIsBurger(true)} className="navbar__burger">
+                <div></div>
+            </div>
+
+            <div className="navbar__logo">
+                {/*<img src={logo} alt=""/>*/}
+                binary
+            </div>
+
+            {
+                isBurger &&
+                <div className="navbar__mobile-menu mobile-menu">
+
+                    <div onClick={() => setIsBurger(false)} className="mobile-menu__cross"></div>
+
+                    <ul className="mobile-menu__list">
+                        <li onClick={() => scrollTo(homeRef)}>home</li>
+                        <li onClick={() => scrollTo(aboutRef)}>about us</li>
+                        <li onClick={() => scrollTo(portfolioRef)}>portfolio</li>
+                        <li onClick={() => scrollTo(contactRef)}>contact</li>
+                    </ul>
+                </div>
+            }
         </aside>
     );
 }
