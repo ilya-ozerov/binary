2d5945fec863c890cddf9cf92b97f84d44668d35
Ilya Ozerov <efwe456lpzx>
2021-10-12 11:54:07 +0300

    added scrollTo func in navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import './navbar.scss';
 
-export const Navbar = () => {
+type NavbarProps = {
+    homeRef: React.RefObject<HTMLDivElement>;
+    aboutRef: React.RefObject<HTMLDivElement>;
+    portfolioRef: React.RefObject<HTMLDivElement>;
+    contactRef: React.RefObject<HTMLDivElement>;
+}
+
+export const Navbar: React.FC<NavbarProps> = ({
+                                                  contactRef, aboutRef,
+                                                  homeRef, portfolioRef
+                                              }) => {
+
+    const scrollTo = (el: React.RefObject<HTMLDivElement>) => {
+        el.current?.scrollIntoView({behavior: "smooth"});
+    }
+
     return (
         <aside className="navbar">
             <ul className="navbar__list">
-                <li>home</li>
-                <li>about us</li>
-                <li>portfolio</li>
-                <li>contact</li>
+                <li onClick={() => scrollTo(homeRef)}>home</li>
+                <li onClick={() => scrollTo(aboutRef)}>about us</li>
+                <li onClick={() => scrollTo(portfolioRef)}>portfolio</li>
+                <li onClick={() => scrollTo(contactRef)}>contact</li>
             </ul>
         </aside>
     );
